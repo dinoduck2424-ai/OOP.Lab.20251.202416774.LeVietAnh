6dbdd3452dbf33473d44f5243fcfab8b006fa7fc
unknown <dinoduck2424@gmail.com>
2025-10-30 19:57:21 +0700

    Fix: Remove temporary setTitle from DigitalVideoDisc

--- Lab02/AimProject02/src/aims/DigitalVideoDisc.java
+++ Lab02/AimProject02/src/aims/DigitalVideoDisc.java
@@ -51,7 +51,7 @@ public class DigitalVideoDisc {
     }
 
   
-    public void setTitle(String title) { this.title = title; }
+    
     public int getId() { return id; }
     public String getTitle() { return title; }
     public String getCategory() { return category; }
